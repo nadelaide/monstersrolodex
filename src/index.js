bf3a72b7f9932bc6e63295984e5e77916af07c88
Nadelaide <nataliepworden@gmail.com>
2021-05-29 22:55:01 -0400

    adding files for github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //looking for a root, using App until root is set
+  //root found in index.html
 );
 
 // If you want to start measuring performance in your app, pass a function
